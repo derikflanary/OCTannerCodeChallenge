cd91d505556ad0c1e0b28d616d919a7ded1f8de6
Derik Flanary <derikflanary@gmail.com>
2016-02-23 21:06:57 -0700

    Initial Commit

---
+++ OCTannerCodeChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OCTannerCodeChallenge
+//
+//  Created by Derik Flanary on 2/23/16.
+//  Copyright © 2016 Derik Flanary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
